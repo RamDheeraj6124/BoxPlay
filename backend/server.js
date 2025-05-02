1935441975f1ac5cdcacffd78df572ecf25af1ed
RamDheeraj6124 <131973519+RamDheeraj6124@users.noreply.github.com>
2025-05-02 11:00:51 +0530

    session

--- backend/server.js
+++ backend/server.js
@@ -96,7 +96,11 @@ app.use(session({
   }),
   domain: '.onrender.com'
 }));
-
+app.use((req, res, next) => {
+  console.log('Session ID:', req.sessionID);
+  console.log('Session:', req.session);
+  next();
+});
 app.use('/user', userroutes);
 app.use('/shop', shoproutes);
 app.use('/admin', adminroutes);
