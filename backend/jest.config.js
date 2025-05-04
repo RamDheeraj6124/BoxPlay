697d7cc834eaed0e31fa01ca32c885a5ab6c2a03
RamDheeraj6124 <131973519+RamDheeraj6124@users.noreply.github.com>
2025-05-05 02:30:35 +0530

    dxcgfbgv b

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: [
       '**/__tests__/**/*.test.js',
-      '**/__tests__/**/*.spec.js',
-      '**/?(*.)+(spec|test).js'
+      '**/*.test.js',
+      '**/*.spec.js',
     ],
     coverageDirectory: 'coverage',
     collectCoverage: true,
