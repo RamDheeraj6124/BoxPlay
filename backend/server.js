cd7369b9a4cd14e491d4afec9ef501d76de86d90
RamDheeraj6124 <131973519+RamDheeraj6124@users.noreply.github.com>
2025-05-05 18:34:42 +0530

    hcbgv

--- backend/server.js
+++ backend/server.js
@@ -137,5 +137,5 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
